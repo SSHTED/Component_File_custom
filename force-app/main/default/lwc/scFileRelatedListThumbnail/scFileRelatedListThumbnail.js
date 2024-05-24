afadb29291ac9640ac9ac59e55890eed876f1283
sshted <sshted5595@email.com>
2024-05-24 13:54:34 +0900

    feat: 썸네일 th 하드코딩 -> dynamic call

--- force-app/main/default/lwc/scFileRelatedListThumbnail/scFileRelatedListThumbnail.js
+++ force-app/main/default/lwc/scFileRelatedListThumbnail/scFileRelatedListThumbnail.js
@@ -6,6 +6,29 @@ export default class ScFileRelatedListThumbnail extends LightningElement {
     @api fileCount;
     @api actNo;
 
+    thumbnailThead = [
+        { label: 'No', fieldName: 'index', sortable: false, type: 'index' },
+        { label: '', fieldName: 'checkbox', sortable: false, type: 'checkbox' },
+        { label: '썸네일', fieldName: 'thumbnail', sortable: false, type: 'thumbnail' },
+        { label: '파일명', fieldName: 'Title', sortable: true, type: 'data' },
+        { label: '확장자', fieldName: 'FileExtension', sortable: true, type: 'data' },
+        { label: '크기', fieldName: 'ContentSize', sortable: true, type: 'data' }
+    ];
+
+    get extendedThumbnailThead(){
+        const result = this.thumbnailThead.map(th => {
+            return {
+                ...th,
+                isIndex: th.type === 'index',
+                isCheckbox: th.type === 'checkbox',
+                isThumbnail: th.type === 'thumbnail',
+                isData: th.type === 'data'
+            };
+        });
+
+        return result;
+    }
+
     handleHeaderCheckboxChange(event) {
         const isChecked = event.target.checked;
         const checkboxes = this.template.querySelectorAll('lightning-input[type="checkbox"]');
