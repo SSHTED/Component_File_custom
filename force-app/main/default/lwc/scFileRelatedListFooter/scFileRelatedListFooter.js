f1d935c5b45942ebeb4981e7ee1f9ecb0becf24c
sshted <sshted5595@email.com>
2024-05-30 15:40:10 +0900

    feat: footer 모두보기 기능 추가

--- force-app/main/default/lwc/scFileRelatedListFooter/scFileRelatedListFooter.js
+++ force-app/main/default/lwc/scFileRelatedListFooter/scFileRelatedListFooter.js
@@ -2,8 +2,17 @@
 import { LightningElement } from 'lwc';
 
 export default class ScFileRelatedListFooter extends LightningElement {
+    get allRecordsUrl() {
+        return this.getUrl('ContentDocument');
+    }
+
+    getUrl(objectApiName) {
+        let baseUrl = window.location.origin;
+        return `${baseUrl}/lightning/o/${objectApiName}/home`;
+    }
+
     handleViewAllClick(event) {
         event.preventDefault();
-        this.dispatchEvent(new CustomEvent('viewallclick'));
+        window.open(this.allRecordsUrl, '_blank');
     }
 }
