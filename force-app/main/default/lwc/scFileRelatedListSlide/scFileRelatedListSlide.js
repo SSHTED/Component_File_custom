80fc627708298521cd9b88acf00bb541ebbf2c14
sshted <sshted5595@email.com>
2024-06-03 13:31:09 +0900

    feat: 슬라이드 탭, 이전, 실행, 다음 버튼 기능 추가, 이미지 onmouseover, onmouseout 기능 추가

--- force-app/main/default/lwc/scFileRelatedListSlide/scFileRelatedListSlide.js
+++ force-app/main/default/lwc/scFileRelatedListSlide/scFileRelatedListSlide.js
@@ -6,32 +6,79 @@ export default class ScFileRelatedListSlide extends LightningElement {
     @api isSmallSizeBox;
     @api selectedRowIds;
 
-    imgSrc = '';
-    imgTitle = '';
+    imgSrc;
+    imgTitle;
+    slideInterval;
     togglePlayIcon = 'utility:play';
+    isPlaying = false;
 
-    handleThumbnailClick(event) {
-        this.imgSrc = event.currentTarget.dataset.imgUrl;
-        this.imgTitle = event.currentTarget.dataset.imgTitle;
+    connectedCallback(){
+
     }
 
     previousImage() {
-        // 이전 이미지로 이동 로직 구현
+        const currentIndex = this.fileData.findIndex(file => file.ImgSrc === this.imgSrc);
+
+        if(currentIndex > 0){
+            // 이전 이미지로 이동
+            const previousFile = this.fileData[currentIndex - 1];
+            this.imgSrc = previousFile.ImgSrc;
+            this.imgTitle = previousFile.Title;
+        }else{
+            // 첫 번째 이미지인 경우 마지막 이미지로 이동
+            const lastFile = this.fileData[this.fileData.length - 1];
+            this.imgSrc = lastFile.ImgSrc;
+            this.imgTitle = lastFile.Title;
+        }
     }
 
     handlePlaying() {
-        // 슬라이드 재생/일시정지 로직 구현
+        if (this.isPlaying) {
+            // 슬라이드 일시정지
+            clearInterval(this.slideInterval);
+            this.isPlaying = false;
+            this.togglePlayIcon = 'utility:play';
+        } else {
+            // 슬라이드 재생
+            this.slideInterval = setInterval(() => {
+                this.nextImage();
+            }, 3000); // 3초마다 다음 이미지로 이동
+            this.isPlaying = true;
+            this.togglePlayIcon = 'utility:pause';
+        }
     }
 
     nextImage() {
-        // 다음 이미지로 이동 로직 구현
+        const currentIndex = this.fileData.findIndex(file => file.ImgSrc === this.imgSrc);
+        if(currentIndex < this.fileData.length - 1) {
+            // 다음 이미지로 이동
+            const nexFile = this.fileData[currentIndex + 1];
+            this.imgSrc = nexFile.ImgSrc;
+            this.imgTitle = nexFile.Title;
+        }else{
+            // 마지막 이미지인 경우 첫 번째 이미지로 이동
+            const firstFile = this.fileData[0];
+            this.imgSrc = firstFile.ImgSrc;
+            this.imgTitle = firstFile.Title;
+        }
     }
+    
+    handleThumbnailClick(event) {
+        // 클릭한 썸네일 이미지의 인덱스 가져오기 (10은 10진법), 일반적으로 HTML 데이터 속성에서 가져오는 값은 문자열 형태
+        const index = parseInt(event.currentTarget.dataset.index, 10);
+        
+        // 해당 인덱스의 이미지로 이동
+        const selectedFile = this.fileData[index];
+        this.imgSrc = selectedFile.ImgSrc;
+        this.imgTitle = selectedFile.Title;
+      }
 
     showImgInfo() {
-        // 슬라이드 이미지 정보 표시 로직 구현
+        this.template.querySelector('.slideImgInfo').style.display = 'block';
+
     }
 
     hideImgInfo() {
-        // 슬라이드 이미지 정보 숨김 로직 구현
+        this.template.querySelector('.slideImgInfo').style.display = 'none';
     }
 }
