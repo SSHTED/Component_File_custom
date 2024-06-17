88a75fa45d26406292d58921146d5cbbed312142
sshted <sshted5595@email.com>
2024-06-17 13:53:34 +0900

    refactor: 테이블 css 클래스 명 수정

--- force-app/main/default/lwc/scFileRelatedListTable/scFileRelatedListTable.js
+++ force-app/main/default/lwc/scFileRelatedListTable/scFileRelatedListTable.js
@@ -17,12 +17,15 @@ export default class ScFileRelatedListTable extends LightningElement {
     // data
     @api fileData;
     @api selectedRowIds;
+
+    //ScFileRelatedListBody 에서 호출하기 위해 @api 추가
     @api resetCheckboxAll() {
         const checkboxes = this.template.querySelectorAll('.checkbox-item');
         checkboxes.forEach(checkbox => {
             checkbox.checked = false;
-          });
+        });
 
+        //to scFileRelatedListContainer
         this.dispatchEvent(new CustomEvent('clearrowids', {
             bubbles: true,
             composed: true
@@ -30,34 +33,12 @@ export default class ScFileRelatedListTable extends LightningElement {
     }
 
     tableThead = [
-        { label: 'No', fieldName: 'index', sortable: false, type: 'index', customClass: 'th1' },
-        { label: '', fieldName: 'checkbox', sortable: false, type: 'checkbox', customClass: 'th2' },
-        { label: '파일명', fieldName: 'Title', sortable: true, type: 'data', customClass: 'th3' },
-        { label: '확장자', fieldName: 'FileExtension', sortable: true, type: 'data', customClass: 'th4' },
-        { label: '크기', fieldName: 'ContentSize', sortable: true, type: 'data', customClass: 'th5' }
+        { label: 'No', fieldName: 'index', sortable: false, type: 'index', customClass: 'slds-col_padded th_index' },
+        { label: '', fieldName: 'checkbox', sortable: false, type: 'checkbox', customClass: 'slds-col_padded th_checkbox' },
+        { label: '파일명', fieldName: 'Title', sortable: true, type: 'data', customClass: 'slds-col_padded th_title' },
+        { label: '확장자', fieldName: 'FileExtension', sortable: true, type: 'data', customClass: 'slds-col_padded th_extension' },
+        { label: '크기', fieldName: 'ContentSize', sortable: true, type: 'data', customClass: 'slds-col_padded th_size' }
     ];
-    
-    /**
-     * 확장된 테이블 헤더 구성을 반환합니다.
-     * actNo가 제공되지 않은 경우 'index' 열을 필터링합니다.
-     * @returns {Array} 확장된 테이블 헤더 구성
-     */
-    get extendedTableThead() {
-        let result = this.tableThead;
-
-        if (!this.actNo) {
-            result = result.filter(th => th.type !== 'index');
-        }
-
-        return result.map(th => {
-            return {
-                ...th,
-                isIndex: th.type === 'index',
-                isCheckbox: th.type === 'checkbox',
-                isData: th.type === 'data'
-            };
-        });
-    }
 
     connectedCallback() {
     }
@@ -66,22 +47,15 @@ export default class ScFileRelatedListTable extends LightningElement {
     }
 
     handleCheckbox(event) {
-        console.log('table handleCheckbox');
-
         const selectedId = event.target.dataset.id;
         const isChecked = event.target.checked;
         const rowCheckboxes = this.template.querySelectorAll('.dataTable tbody lightning-input');
-        const allChecked = Array.from(rowCheckboxes).every(checkbox => checkbox.checked);
-
         const headerCheckbox = this.template.querySelector('.dataTable thead lightning-input');
+        const allChecked = Array.from(rowCheckboxes).every(checkbox => checkbox.checked);
 
         if (headerCheckbox) {
             headerCheckbox.checked = allChecked;
         }
-
-        console.log('child selectedId: ', selectedId);
-        console.log('child isChecked: ', isChecked);
-        console.log('child allChecked: ', allChecked);
 
         this.dispatchEvent(new CustomEvent('checkboxchange', {
             detail: { selectedId, isChecked },
@@ -91,7 +65,6 @@ export default class ScFileRelatedListTable extends LightningElement {
     }
 
     handleCheckboxAll(event) {
-        console.log('table handleCheckboxAll');
         const selectedIds = [];
         const isChecked = event.target.checked;
         const rowCheckboxes = this.template.querySelectorAll('.dataTable tbody lightning-input');
@@ -107,5 +80,27 @@ export default class ScFileRelatedListTable extends LightningElement {
             bubbles: true,
             composed: true
         }));
+    }
+
+    /**
+     * 확장된 테이블 헤더 구성을 반환합니다.
+     * actNo가 제공되지 않은 경우 'index' 열을 필터링합니다.
+     * @returns {Array} 확장된 테이블 헤더 구성
+    */
+    get extendedTableThead() {
+        let result = this.tableThead;
+
+        if (!this.actNo) {
+            result = result.filter(th => th.type !== 'index');
+        }
+
+        return result.map(th => {
+            return {
+                ...th,
+                isIndex: th.type === 'index',
+                isCheckbox: th.type === 'checkbox',
+                isData: th.type === 'data'
+            };
+        });
     }
 }
